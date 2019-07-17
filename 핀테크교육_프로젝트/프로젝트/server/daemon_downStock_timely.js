5cdaa0498bcf9e731887d06d4254a12540002947
Ubuntu <ubuntu@ip-172-31-35-104.ec2.internal>
2019-07-17 02:26:20 +0000

    update

--- 핀테크교육_프로젝트/프로젝트/server/daemon_downStock_timely.js
+++ 핀테크교육_프로젝트/프로젝트/server/daemon_downStock_timely.js
@@ -1,12 +1,10 @@
 const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
-let driver = new webdriver.Builder()
-    .forBrowser('firefox')
-    .setChromeOptions(/* ... */)
-    .setFirefoxOptions(/* ... */)
+const driver = new webdriver.Builder()
+    .forBrowser('chrome')
     .build();
 
-const url = 'http://54.80.34.62:5555/index.ejs';
-//html을 업로드한 경로에서 불러온다
+//const url = 'http://54.80.34.62:5555/index.ejs';
+const url = 'https://www.naver.com';
 driver.get(url);
