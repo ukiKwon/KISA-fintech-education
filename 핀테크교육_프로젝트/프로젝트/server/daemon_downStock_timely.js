4390d0a6d526b9f512a7ba764131d2b85e3230de
snrndi121 <snrndi4521@gmail.com>
2019-07-17 11:25:20 +0900

    server

--- 핀테크교육_프로젝트/프로젝트/server/daemon_downStock_timely.js
+++ 핀테크교육_프로젝트/프로젝트/server/daemon_downStock_timely.js
@@ -2,11 +2,9 @@ const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
 let driver = new webdriver.Builder()
-    .forBrowser('firefox')
-    .setChromeOptions(/* ... */)
-    .setFirefoxOptions(/* ... */)
+    .forBrowser('chrome')
     .build();
 
-const url = 'http://54.80.34.62:5555/index.ejs';
+const url = 'http://marketdata.krx.co.kr/mdi#document=040402/';
 //html을 업로드한 경로에서 불러온다
 driver.get(url);
